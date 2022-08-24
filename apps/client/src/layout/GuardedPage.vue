71ad823a0d12e1406ed5499d2c4e11334b9c048f
James Sinkala <xinnks@gmail.com>
2022-08-24 22:24:04 +0300

    feat: Add guarded pages layout component

---
+++ apps/client/src/layout/GuardedPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavBar from "./../components/NavBar.vue";
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <nav-bar></nav-bar>
+    <div class="w-full">
+      <slot></slot>
+    </div>
+  </div>
+</template>
